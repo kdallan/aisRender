a100d85f727612f4971898c052d7eb5c569ad4b3
kdallan <kdallan@hotmail.com>
2025-03-14 13:45:55 -0700

    Revert to single channel

--- server.js
+++ server.js
@@ -159,8 +159,8 @@ const setupDeepgram = (mediaStream) => {
     // Audio
     encoding: "mulaw",
     sample_rate: 8000,
-    channels: 2,
-    multichannel: true,
+    channels: 1,
+    multichannel: false,
     // End of Speech
     no_delay: true,
     interim_results: true,
