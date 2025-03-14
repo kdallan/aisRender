613ce8ae275a2833a137346732895c891bd5a281
kdallan <kdallan@hotmail.com>
2025-03-14 15:55:45 -0700

    Add hangup

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ const twilio = require("twilio"); // Or, for ESM: import twilio from "twilio";
 
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
-const accountSid = "AC0f8df3d52746ca7992d4f0b9c74a0b8e"; // process.env.TWILIO_ACCOUNT_SID;
-const authToken = "f5a41d422345b5c6ffce75c12664fcb0"; // process.env.TWILIO_AUTH_TOKEN;
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
 // Twilio
