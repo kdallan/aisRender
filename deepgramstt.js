32ff48fc7b9393a4dc7a4861a8b18cdc0aab9a57
kdallan <kdallan@hotmail.com>
2025-03-24 15:55:34 -0700

    add log

--- deepgramstt.js
+++ deepgramstt.js
@@ -1,5 +1,13 @@
 const { createClient, LiveTranscriptionEvents } = require( "@deepgram/sdk" );
 
+// Simplified logger
+const log = {
+    info: (msg, data) => console.log(`${msg}`, data || ""),
+    error: (msg, err) => console.error(`[ERROR] ${msg}`, err || ""),
+    warn: (msg, data) => console.warn(`[WARN] ${msg}`, data || ""),
+    debug: (msg, data) => process.env.DEBUG && console.log(`[DEBUG] ${msg}`, data || "")
+};
+
 // DeepgramSTTService
 class DeepgramSTTService {
     constructor(config, onTranscript, onUtteranceEnd) {
